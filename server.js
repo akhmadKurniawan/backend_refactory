7ef1a0d3b4b5ef89be6d088a36c5b34f798ac6ec
akhmadkurniawan <akhmad.k97@gmail.com>
2021-12-01 09:39:15 +0800

    auth

--- server.js
+++ server.js
@@ -1,14 +1,47 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
 
 //Load env vars
 dotenv.config({ path: './config/config.env' });
 
+// Connect to databse
+connectDB();
+
+//Route files
+const auth = require('./routes/auth');
+
 const app = express();
+
+// Body parser
+app.use(express.json());
+
+// Cookie parser
+app.use(cookieParser());
+
+//Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/auth', auth);
+
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
 	PORT,
 	console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise) => {
+	console.log(`Error: ${err.message}`);
+	// Close server & exit server
+	server.close(() => process.exit(1));
+});
